e967e46af32ca1969362d7098fb91c5eff2eec83
MtheMartian <marvin.altidor@outlook.com>
2022-10-27 22:01:46 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import './General.css';
+import './HowWeFeelin.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="moodBox">
+        <div className="feelText">HOW WE FEELING?</div>
+      <div>
+        <input id="userMood" type="text" placeholder="Mood" />
+      </div>
+      </div>
+    </main>
   );
 }
 
